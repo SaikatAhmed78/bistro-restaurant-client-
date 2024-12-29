9d1857dfc28ffb9272eec9121353d713c6ffa109
SaikatAhmed78 <shaikatahmed78@gmail.com>
2024-12-30 01:26:10 +0600

    Add responsive banner and improve navbar UI for better user experience

--- src/Common/Navbar.jsx
+++ src/Common/Navbar.jsx
@@ -4,52 +4,68 @@ import { FaHome, FaPhoneAlt, FaTachometerAlt, FaUtensils, FaStore, FaSignOutAlt 
 
 const Navbar = () => {
 
-    const NavItem = ({ to, icon, children }) => (
-        <li>
+    const NavItem = ({ to, icon, label }) => (
+        <li className="text-lg">
             <NavLink
                 to={to}
-                className={({ isActive }) => isActive ? "text-yellow-300 flex items-center" : "flex items-center"}
+                className={({ isActive }) => 
+                    `flex items-center space-x-2 p-2 rounded-md transition-all duration-300 ${
+                        isActive ? "text-yellow-300 bg-gray-800" : "text-gray-400 hover:text-white hover:bg-gray-700"
+                    }`
+                }
             >
-                {React.cloneElement(icon, { className: 'mr-2' })}
-                {children}
+                {React.cloneElement(icon, { className: 'w-5 h-5' })}
+                <span>{label}</span>
             </NavLink>
         </li>
     );
 
     return (
-        <div className="navbar bg-gray-900 text-white">
-            <div className="navbar-start">
-                <div className="dropdown">
-                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
-                        </svg>
-                    </label>
-                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-gray-900 rounded-box w-52">
-                        <NavItem to="/" icon={<FaHome />}>Home</NavItem>
-                        <NavItem to="/contact" icon={<FaPhoneAlt />}>Contact Us</NavItem>
-                        <NavItem to="/dashboard" icon={<FaTachometerAlt />}>Dashboard</NavItem>
-                        <NavItem to="/menu" icon={<FaUtensils />}>Our Menu</NavItem>
-                        <NavItem to="/shop" icon={<FaStore />}>Our Shop</NavItem>
-                        <NavItem to="/signout" icon={<FaSignOutAlt />}>Sign Out</NavItem>
+        <nav className="bg-black bg-opacity-70 fixed w-full z-50 shadow-lg">
+            <div className="container mx-auto flex justify-between items-center p-4">
+
+                <Link to="/" className="text-2xl font-bold text-white tracking-wide">
+                    BISTRO BOSS <br />
+                    <span className="text-yellow-400">RESTAURANT</span>
+                </Link>
+
+                <div className="hidden lg:flex space-x-8">
+                    <ul className="flex space-x-6">
+                        <NavItem to="/" icon={<FaHome />} label="Home" />
+                        <NavItem to="/contact" icon={<FaPhoneAlt />} label="Contact Us" />
+                        <NavItem to="/dashboard" icon={<FaTachometerAlt />} label="Dashboard" />
+                        <NavItem to="/menu" icon={<FaUtensils />} label="Our Menu" />
+                        <NavItem to="/shop" icon={<FaStore />} label="Our Shop" />
+                        <NavItem to="/signout" icon={<FaSignOutAlt />} label="Sign Out" />
                     </ul>
                 </div>
-                <a className="btn btn-ghost normal-case text-xl">BISTRO BOSS <br /> RESTAURANT</a>
+
+                <div className="lg:hidden">
+                    <div className="dropdown relative">
+                        <label tabIndex={0} className="btn btn-ghost">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+                            </svg>
+                        </label>
+                        <ul
+                            tabIndex={0}
+                            className="dropdown-content mt-3 p-4 space-y-3 shadow bg-gray-900 rounded-md w-52"
+                        >
+                            <NavItem to="/" icon={<FaHome />} label="Home" />
+                            <NavItem to="/contact" icon={<FaPhoneAlt />} label="Contact Us" />
+                            <NavItem to="/dashboard" icon={<FaTachometerAlt />} label="Dashboard" />
+                            <NavItem to="/menu" icon={<FaUtensils />} label="Our Menu" />
+                            <NavItem to="/shop" icon={<FaStore />} label="Our Shop" />
+                            <NavItem to="/signout" icon={<FaSignOutAlt />} label="Sign Out" />
+                        </ul>
+                    </div>
+                </div>
+
+                <Link to="/signup" className="btn bg-yellow-500 hover:bg-yellow-400 text-black">
+                    Sign Up
+                </Link>
             </div>
-            <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal p-0">
-                    <NavItem to="/" icon={<FaHome />}>Home</NavItem>
-                    <NavItem to="/contact" icon={<FaPhoneAlt />}>Contact Us</NavItem>
-                    <NavItem to="/dashboard" icon={<FaTachometerAlt />}>Dashboard</NavItem>
-                    <NavItem to="/menu" icon={<FaUtensils />}>Our Menu</NavItem>
-                    <NavItem to="/shop" icon={<FaStore />}>Our Shop</NavItem>
-                    <NavItem to="/signout" icon={<FaSignOutAlt />}>Sign Out</NavItem>
-                </ul>
-            </div>
-            <div className="navbar-end">
-                <Link className="btn">Sign Up</Link>
-            </div>
-        </div>
+        </nav>
     );
 };
 
--- src/Main/MainLayout.jsx
+++ src/Main/MainLayout.jsx
@@ -6,9 +6,11 @@ import Footer from '../Common/Footer';
 const MainLayout = () => {
     return (
         <div>
+            <div className='mx-auto'>
             <Navbar></Navbar>
+            </div>
 
-            <div className='w-11/12 mx-auto min-h-[calc(100vh-306px)]'>
+            <div className='min-h-[calc(100vh-306px)]'>
             <Outlet></Outlet>
             </div>
 
